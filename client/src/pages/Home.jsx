1ecfb74d1774b6d35220defb137a07a46c026f98
geoquimica-gomez <geoquimica.gomez@gmail.com>
2025-01-14 22:57:45 -0300

    componente carrusel listo

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,7 +1,9 @@
 import SiteUnderConstruction from "./SiteUnderConstruction.jsx";
+import Carousel from "../components/Carousel.jsx";
 function Home() {
     return (
         <div>
+            <Carousel />
             <SiteUnderConstruction />
         </div>
     );
